bcfca9fa45a8665e89ab1f1760456cf03ad5590c
PariSh KhAn <xttrra@gmail.com>
2017-11-13 20:39:14 +0600

    Add SASS and NodeStream Support

---
+++ razzle.config.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const autoprefixer = require("autoprefixer");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Visualizer = require("webpack-visualizer-plugin");
+
+module.exports = {
+  modify: (baseConfig, { target, dev }, webpack) => {
+    const appConfig = Object.assign({}, baseConfig);
+
+    // Setup SCSS
+    if (target === "web") {
+      const cssLoader = {
+        loader: "css-loader",
+        options: {
+          minimize: !dev,
+          sourceMap: dev,
+          importLoaders: 1
+        }
+      };
+
+      const postCSSLoader = {
+        loader: "postcss-loader",
+        options: {
+          ident: "postcss", // https://webpack.js.org/guides/migrating/#complex-options
+          sourceMap: dev,
+          plugins: () => [
+            autoprefixer({
+              browsers: [
+                ">1%",
+                "last 4 versions",
+                "Firefox ESR",
+                "not ie < 9" // React doesn't support IE8 anyway
+              ]
+            })
+          ]
+        }
+      };
+
+      const sassLoader = {
+        loader: "sass-loader",
+        options: {
+          sourceMap: dev
+        }
+      };
+
+      if (dev) {
+        // For development, include source map
+        appConfig.module.rules.push({
+          test: /.scss$/,
+          use: ["style-loader", cssLoader, postCSSLoader, sassLoader]
+        });
+      } else {
+        // For production, extract CSS
+        appConfig.module.rules.push({
+          test: /.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: [cssLoader, postCSSLoader, sassLoader]
+          })
+        });
+
+        appConfig.plugins.push(
+          new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+          new webpack.IgnorePlugin(/moment/, /react-kronos/),
+          new Visualizer(),
+          new ExtractTextPlugin( "bundle.css" )
+        );
+      }
+    } else {
+      appConfig.module.rules.push({
+        test: /.scss$/,
+        use: ["ignore-loader"]
+      });
+    }
+
+    return appConfig;
+  }
+};
